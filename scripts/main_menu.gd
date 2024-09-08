8932ffa0369ec894663f3a132a5429f1a621c9b7
digiur <digiur@digiur.dev>
2024-09-08 14:07:37 -0600

    default demo level

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,4 @@
 extends CanvasLayer
 
 func _on_start_button_pressed() -> void:
-	WD.start_level("res://scenes/levels/level_demo.tscn")
+	WD.start_level("res://scenes/encounters/encounter_demo.tscn")
