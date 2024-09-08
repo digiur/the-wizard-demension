5cd473075048d2251cf70cc2b6100ec957bf30c3
digiur <digiur@digiur.dev>
2024-09-07 21:46:09 -0600

    remove files log

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,9 +2,3 @@ extends CanvasLayer
 
 func _on_start_button_pressed() -> void:
 	WD.start_level("res://scenes/levels/level_demo.tscn")
-
-func _on_options_button_pressed() -> void:
-	pass
-
-func _on_about_button_pressed() -> void:
-	pass
--- scripts/static_bus.gd
+++
@@ -1,2 +0,0 @@
-class_name StaticBus
-extends Object
